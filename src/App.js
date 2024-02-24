9bcdcd0974656bd9922adf7ffe86db76cb8b01c4
Nhan <nhantrong3123@gmail.com>
2024-02-24 20:10:08 +0700

    add story paragrah

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ function App() {
   const colors = ['red', 'green', 'yellow', 'blue', 'white', 'black', 'pink', 'purple', 'orange']
 
   return (
-  <div className='App' >
+  <div className='cyber-container' >
     <h2>Good {timeOfDay}! </h2>
-    <b>{colors}</b>
+    <b>{colors + ' '}</b>
+    <p>This is static web for test purpose only</p>
+    <p>Lần đầu tiên chúng tôi gặp nhau là khi tôi vừa bước chân vào con đường đại học. Khi đó, một ngày đầu xuân, không lời chào hỏi, không cầu kì, nhanh thoanh thoắt, thoáng chốc ngồi vào đối diện tôi. Bốn mắt nhìn nhau, vẫn không một lời nhưng có lẽ như cả hai đã thấu hiểu nhau từ rất lâu, tôi chia sẻ chiếc xúc xích đang ăn dở của mình. Cả hai cùng ngồi xuống hoàn thành nốt phần ăn của mình. Cứ như thế, nhẹ nhàng, êm dịu, chúng tôi đã gặp nhau như thế.</p>
   </div>
   );
 }
